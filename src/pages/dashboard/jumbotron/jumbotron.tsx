9bb356a87bed093c8d55c5a4c25d364ad9008d1c
jacob0123456 <jacobroberto950212@gmail.com>
2022-11-17 05:38:48 +0900

    structure complete

--- src/pages/dashboard/jumbotron/jumbotron.tsx
+++ src/pages/dashboard/jumbotron/jumbotron.tsx
@@ -6,7 +6,13 @@ import "./jumbotron.scss";
 const Jumbotron = () => {
   return (
     <div className="jumbotron">
-      <h1>HALIFAX WORDPRESS & WEB APP DEVELOPMENT</h1>
+      <div className="content">
+        <h1>HALIFAX WORDPRESS & WEB APP DEVELOPMENT</h1>
+        <h2>
+          Specializing in custom theme development. If you're a business seeking
+          a web presence or are looking to hire, contact me <span>here</span>
+        </h2>
+      </div>
     </div>
   );
 };
