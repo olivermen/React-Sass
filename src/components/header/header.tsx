677f88633fd8e3a94b0364308f19aa74142dd0da
jacob0123456 <jacobroberto950212@gmail.com>
2022-11-17 00:51:16 +0900

    finish PM

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,14 @@ import "./header.scss";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <div className="title">
+        <a href="https://jonny.me">jonny.me</a>
+      </div>
+      <div className="navbar">
+        <a href="https://jonny.me">SERVICES</a>
+        <a href="https://jonny.me">PORTFOLIO</a>
+        <a href="https://jonny.me">PRICING</a>
+      </div>
     </div>
   );
 };
