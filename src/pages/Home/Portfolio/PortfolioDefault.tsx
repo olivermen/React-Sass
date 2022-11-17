4a9a32abbb659b41cc443020802beaea45a616ce
jacob0123456 <jacobroberto950212@gmail.com>
2022-11-17 18:56:21 +0900

    portfolio

--- src/pages/Home/Portfolio/PortfolioDefault.tsx
+++ src/pages/Home/Portfolio/PortfolioDefault.tsx
@@ -2,11 +2,55 @@ import React from "react";
 
 // import stylesheets
 import "./portfolio.scss";
+import portfolio1 from "../../../assets/images/portfolio1.png";
 
-const PortfolioDefault = () => {
+// import button components
+import Button from "../../../components/Buttons";
+
+interface PortfolioDefaultProps {
+  screen: string;
+  title: string;
+  badge: string;
+  description: string;
+  design: boolean;
+}
+
+const PortfolioDefault = ({
+  screen,
+  title,
+  badge,
+  description,
+  design,
+}: PortfolioDefaultProps) => {
   return (
-    <div className="contact">
-      <h1>Portfolio</h1>
+    <div className="default">
+      <div className="title">
+        <h1>{design ? "PORTFOLIO" : ""}</h1>
+      </div>
+      <div className="content">
+        <div className="portfolioImage">
+          <img
+            src={require(`../../../assets/images/${screen}`)}
+            width={570}
+            height={303}
+            alt="portfolio1"
+          ></img>
+        </div>
+        <div className="description">
+          <h2>
+            {title}
+            <span className="badge">{badge}</span>
+          </h2>
+          <h3>{description}</h3>
+          <h4 hidden={!design}>
+            Designed by <a href="https://jonny.me">Kristin Killam</a>
+          </h4>
+          <div className="buttonGroup">
+            <Button type="btnDefault" title="Preview" />
+            <Button type="btnSecondary" title="Visit site" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
